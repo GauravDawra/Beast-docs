1b7b20a456ed4266e7d60fdc31be47a49aa9b815
GauravDawra <gaurav19039@iiitd.ac.in>
2022-06-04 21:17:41 +0530

    workflows

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/Beast-docs/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.png',
